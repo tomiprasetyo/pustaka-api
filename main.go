4824fb938a3fd7139acc95e4d290d0ba1f7029f4
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-04-06 22:22:48 +0700

    Path variable & query string parameter

--- main.go
+++ main.go
@@ -9,12 +9,31 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"name":   "Tomi Prasetyo",
-			"status": "OK",
-		})
-	})
+	router.GET("/", RootHandler)
+	router.GET("/books/:id", BooksHandler)
+	router.GET("/books/:id/:title", BooksHandler)
+	router.GET("/query", QueryHandler)
 
 	router.Run()
+}
+
+func RootHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"name":   "Tomi Prasetyo",
+		"status": "OK",
+	})
+}
+
+func BooksHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+	title := ctx.Param("title")
+
+	ctx.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func QueryHandler(ctx *gin.Context) {
+	title := ctx.Query("title")
+	price := ctx.Query("price")
+
+	ctx.JSON(http.StatusOK, gin.H{"title": title, "price": price})
 }
