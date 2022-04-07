aea93ca7a367cb6dbd5349e3617a59b5e0544df9
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-04-07 22:20:04 +0700

    menggunakan gorm, membuat entity & koneksi ke database

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success connecting database")
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
